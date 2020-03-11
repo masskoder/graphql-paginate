34509ec7bcec7b734492ff36cb38859413c7a93f
Cletus RAZAKOU <masskoder@gmail.com>
2020-03-11 00:55:50 +0000

    set up and tested pagination using jest

---
+++ test.js
@@ -0,0 +1,41 @@
+const graphqlPagination = require("./index");
+
+describe("pagination", () => {
+  const models = [
+    {
+      id: 1,
+      name: "user 1"
+    },
+    {
+      id: 2,
+      name: "user 2"
+    },
+    {
+      id: 3,
+      name: "user 3"
+    },
+    {
+      id: 4,
+      name: "user 4"
+    },
+    {
+      id: 5,
+      name: "user 5"
+    }
+  ];
+  const mockModel = {
+    findAll: jest.fn(() => models),
+    count: jest.fn(() => models.length)
+  };
+
+  it("call skip , first and limit args ", async () => {
+    const args = {
+      skip: 2,
+      first: 2,
+      LIMIT: 2
+    };
+    const pagination = await graphqlPagination(mockModel, args, "id");
+
+    expect(pagination).toMatchSnapshot();
+  });
+});
